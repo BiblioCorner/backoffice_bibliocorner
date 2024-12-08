f81d3a84be48fdccf40e86547e252383323f541c
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2024-12-08 23:33:23 +0100

    ✨ Enhance Events page with a detailed table of event information and registration links

--- src/app/catalog/events/page.js
+++ src/app/catalog/events/page.js
@@ -1,9 +1,80 @@
-import React from 'react'
+"use client";
+import Table from "@/components/ui/Table";
+import { createColumnHelper } from "@tanstack/react-table";
+import Link from "next/link";
+import React from "react";
 
 const EventsPage = () => {
-  return (
-    <div>EventsPage</div>
-  )
-}
+  const events = [
+    {
+      id: "1",
+      name: "Book Reading",
+      description: "An event for book lovers",
+      date: "2023-10-01",
+      start_time: "10:00",
+      end_time: "12:00",
+      library_id: "lib123",
+      address: "123 Library St.",
+      register_link: "http://example.com/register",
+    },
+    {
+      id: "2",
+      name: "Author Meet and Greet",
+      description: "Meet your favorite authors",
+      date: "2023-10-05",
+      start_time: "14:00",
+      end_time: "16:00",
+      library_id: "lib456",
+      address: "456 Library Ave.",
+      register_link: "http://example.com/register",
+    },
+  ];
 
-export default EventsPage
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.accessor("id", {
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("name", {
+      header: "Nom",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("description", {
+      header: "Description",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("date", {
+      header: "Date",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("start_time", {
+      header: "Heure de début",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("end_time", {
+      header: "Heure de fin",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("library_id", {
+      header: "Bibliothèque",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("address", {
+      header: "Adresse",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor("register_link", {
+      header: "Lien d'inscription",
+      cell: (info) => <Link href={info.getValue()}>{info.getValue()}</Link>,
+    }),
+  ];
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Gestion des évènements</h1>
+      <Table columns={columns} data={events} />
+    </>
+  );
+};
+
+export default EventsPage;
