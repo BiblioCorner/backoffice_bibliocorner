11f54500b5a55388ef018a45e957238d28cc55b1
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2024-12-08 22:54:10 +0100

    ✨ Enhance Sidebar component with additional icons and links for moderation

--- src/components/partials/Sidebar/index.js
+++ src/components/partials/Sidebar/index.js
@@ -1,4 +1,12 @@
-import { Calendar, House, LayoutDashboard, LibraryBig, Users } from "lucide-react";
+import {
+  Calendar,
+  House,
+  LayoutDashboard,
+  LibraryBig,
+  MessageCircleMore,
+  MessagesSquare,
+  Users,
+} from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -8,35 +16,70 @@ const Sidebar = () => {
       <nav className="h-full">
         <ul>
           <li>
-            <Link href="#" className="flex items-center gap-2 p-4 hover:bg-gray-200">
+            <Link
+              href="#"
+              className="flex items-center gap-2 p-4 hover:bg-gray-200"
+            >
               <LayoutDashboard size={16} />
               Tableau de bord
             </Link>
           </li>
           <li>
-            <Link href="#" className="flex items-center gap-2 p-4 hover:bg-gray-200">
+            <Link
+              href="#"
+              className="flex items-center gap-2 p-4 hover:bg-gray-200"
+            >
               <House size={16} />
               Catalogue
             </Link>
             <ul className="pl-4">
               <li>
-                <Link href="#" className="flex items-center gap-2 p-4 hover:bg-gray-200">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 p-4 hover:bg-gray-200"
+                >
                   <Users size={16} />
                   Utilisateurs
                 </Link>
               </li>
               <li>
-                <Link href="#" className="flex items-center gap-2 p-4 hover:bg-gray-200">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 p-4 hover:bg-gray-200"
+                >
                   <LibraryBig size={16} />
                   Bibliothèques
                 </Link>
               </li>
               <li>
-                <Link href="#" className="flex items-center gap-2 p-4 hover:bg-gray-200">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 p-4 hover:bg-gray-200"
+                >
                   <Calendar size={16} />
-                    Événements
+                  Événements
                 </Link>
-              </li>             
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link
+              href="#"
+              className="flex items-center gap-2 p-4 hover:bg-gray-200"
+            >
+              <MessagesSquare size={16} />
+              Modération
+            </Link>
+            <ul className="pl-4">
+              <li>
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 p-4 hover:bg-gray-200"
+                >
+                  <MessageCircleMore size={16} />
+                  Commentaires
+                </Link>
+              </li>
             </ul>
           </li>
         </ul>
